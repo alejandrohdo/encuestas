4caecb589b24d1f12dbace1c80c4863f07ad62aa
iisotec <iisotecperu@gmail.com>
2016-08-04 10:51:34 -0500

    primera version del sistema

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,7 @@
+<li class="{{ Request::is('home*') ? 'active' : '' }}">
+   	<a href="{!! url('/home') !!}">Inicio</a>	
+	<a href="{!! url('/estudiantes') !!}">Estudiantes</a>	
+   	<a href="{!! url('/carrara_profesional') !!}">Carreras profesionales</a>	
+   	<a href="{!! url('/usuario') !!}">Usuarios</a>	
+   
+</li>
